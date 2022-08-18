46a40c49bf50a6f853b859164c34e22b288b9833
Bryton Haldey <bryton.hadley@gmail.com>
2022-08-18 13:04:09 -0600

    1st commit

--- arrays.js
+++ arrays.js
@@ -1,4 +1,39 @@
 /*
     In this file, we'll be continuing our story
-    with Jon being allowed to flee his fight with Jamie.
+    with Obiwan being allowed to flee his fight with Anakin.
 */
+
+let backpack = []
+
+backpack.push('Jedi Robe')
+backpack.push('Lightsaber')
+backpack.push('Crippling sense of failure and guilt')
+
+//console.log(backpack)
+
+backpack.splice(1,1)
+
+//console.log(backpack)
+
+backpack.push('Wookie Coat') 
+
+//console.log(backpack)
+
+backpack.pop()
+
+//console.log(backpack)
+
+backpack.push('Blue milk','baby yoda', 'luke', 'high ground', 'blaster')
+// second backpack and how to make and put stuff in 
+let stroller = backpack.splice(3,2)
+
+console.log(backpack)
+console.log(stroller)
+
+
+// Loop over backpack array
+// Console log each item
+
+for(let i = 0; i < backpack.length; i++){
+    console.log(backpack[i])
+}
